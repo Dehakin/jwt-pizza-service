e5b81c65b7b359cfceebb2a1184b53cce8204320
Dehakin <zachary@penneylaw.com>
2024-11-12 20:14:55 -0700

    attempt fix with bind

--- src/service.js
+++ src/service.js
@@ -13,7 +13,7 @@ app.use(setAuthUser);
 // metrics middleware
 app.use(metrics.httpTracker);
 app.use(metrics.authenticationTracker);
-app.use(metrics.pizzaTracker);
+app.use(metrics.pizzaTracker.bind(metrics));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
